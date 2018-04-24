ac48444f4df96e5c921b41600cc571543337603a
Rajagopal92 <raja.g@invenzolabs.com>
2018-04-24 12:38:53 +0530

    bug fixes

--- apiservices/src/main/java/com/libRG/apiService/raja/ApiService.java
+++ apiservices/src/main/java/com/libRG/apiService/raja/ApiService.java
@@ -178,7 +178,7 @@ public class ApiService {
         params = param;
     }
 
-    public HashMap<String, String> getHeaders() {
+    public static HashMap<String, String> getHeaders() {
         return params;
     }
 
